e622f17fd3458b731e9588f66ba6adb776382f8e
Felipe Engelberger <felipe.engelberger@ug.uchile.cl>
2023-02-11 22:40:09 +0100

    comments

--- paperqa/docs.py
+++ paperqa/docs.py
@@ -1,4 +1,4 @@
-from typing import List, Optional, Tuple, Union
+from typing import Any, Dict, List, Optional, Tuple, Union
 from functools import reduce
 import re
 from .utils import maybe_is_text, maybe_is_truncated
@@ -7,7 +7,7 @@ from dataclasses import dataclass
 from .readers import read_doc
 from langchain.vectorstores import FAISS
 from langchain.embeddings.openai import OpenAIEmbeddings
-
+import os
 
 @dataclass
 class Answer:
@@ -24,10 +24,15 @@ class Docs:
     """A collection of documents to be used for answering questions."""
 
     def __init__(self, chunk_size_limit: int = 3000) -> None:
+        if not isinstance(chunk_size_limit, int):
+            raise TypeError("chunk_size_limit should be an integer")
+        if chunk_size_limit < 0:
+            raise ValueError("chunk_size_limit should be a positive integer")
         self.docs = dict()
         self.chunk_size_limit = chunk_size_limit
         self.keys = set()
         self._faiss_index = None
+
 
     def add(
         self,
@@ -36,7 +41,17 @@ class Docs:
         key: Optional[str] = None,
         disable_check: bool = False,
     ) -> None:
-        """Add a document to the collection."""
+        """Add a document to the collection.
+        Args:
+            path (str): Path to the document (e.g. a PDF).
+            citation (str): Citation of the document, e.g. "Smith 2020".
+            key (str, optional): Key to refer to the document. If not specified, we
+                try to extract it from the citation. Defaults to None.
+            disable_check (bool, optional): Disable the check that the document
+                has been loaded correctly. Defaults to False.
+        """
+        if not os.path.exists(path):
+            raise ValueError(f"Path {path} not found.")
         if path in self.docs:
             raise ValueError(f"Document {path} already in collection.")
         if key is None:
@@ -79,51 +94,93 @@ class Docs:
         if self._faiss_index is None:
             self._build_faiss_index()
         state = self.__dict__.copy()
-        state["_faiss_index"].save_local("faiss_index")
-        del state["_faiss_index"]
+        try:
+            state["_faiss_index"].save_local("faiss_index")
+            del state["_faiss_index"]
+        except:
+            print("Error in saving faiss index")
         return state
 
-    def __setstate__(self, state):
+    def __setstate__(self, state: Dict[str, Any]) -> None:
+        # Update the state of the class with the state that was passed in.
+        # This is a standard way to restore the state of a class.
         self.__dict__.update(state)
+        # Load the FAISS index from the state.
         self._faiss_index = FAISS.load_local("faiss_index", OpenAIEmbeddings())
 
-    def _build_faiss_index(self):
+
+    def _build_faiss_index(self) -> None:
+        """
+        A method to build a FAISS index of the documents in the collection.
+        In order to do this, we need to create a list of all the texts in our documents 
+        and a list of all the metadata in our documents.
+        We then create a new FAISS index using the FAISS class from haystack and the “from_texts” method.
+        """
         if self._faiss_index is None:
-            texts = reduce(
+            # create a list of all the texts in our documents
+            texts: List[str] = reduce(
                 lambda x, y: x + y, [doc["texts"] for doc in self.docs.values()], []
             )
-            metadatas = reduce(
+            # create a list of all the metadata in our documents
+            metadatas: List[Dict[str, Any]] = reduce(
                 lambda x, y: x + y, [doc["metadata"] for doc in self.docs.values()], []
             )
-            self._faiss_index = FAISS.from_texts(
-                texts, OpenAIEmbeddings(), metadatas=metadatas
-            )
+            try:
+                # create a new FAISS index using the FAISS class from haystack and the “from_texts” method.
+                self._faiss_index = FAISS.from_texts(
+                    texts, OpenAIEmbeddings(), metadatas=metadatas
+                )
+            except TypeError:
+                self._faiss_index = None
 
-    def get_evidence(self, question: str, k: int = 3, max_sources: int = 5):
+
+    def get_evidence(self, question: str, k: int = 3, max_sources: int = 5) -> Tuple[str, Dict[str, str]]:
+        """
+        A method to get the evidence for a given question.
+        We first use the FAISS index to find the documents that are most relevant to the question.
+        We then use the distill chain to extract the relevant sentences from each document.
+        We then return the context string and the citations for the documents.
+        
+        Args:
+            question (str): The question to be answered.
+            k (int, optional): The number of documents to be returned. Defaults to 3.
+            max_sources (int, optional): The maximum number of sources to be returned. Defaults to 5.
+        """        
+        # Step 1: Initialize context
         context = []
+        # Step 2: If self._faiss_index is None, build the faiss index
         if self._faiss_index is None:
             self._build_faiss_index()
-        # want to work through indices but less k
+        # Step 3: Get the top k documents from the faiss index
         docs = self._faiss_index.max_marginal_relevance_search(
             question, k=k, fetch_k=5 * k
         )
+        # Step 4: For each document, run distill chain
         for doc in docs:
+            # want to work through indices but less k
             c = (
                 doc.metadata["key"],
                 doc.metadata["citation"],
                 distill_chain.run(question=question, context_str=doc.page_content),
             )
+            # Step 5: If the context is not empty, append it to context
             if "Not applicable" not in c[-1]:
                 context.append(c)
+            # Step 6: If the length of the context is equal to max_sources, break the loop
             if len(context) == max_sources:
                 break
+        # Step 7: Concatenate the context strings
         context_str = "\n\n".join(
             [f"{k}: {s}" for k, c, s in context if "Not applicable" not in s]
         )
+        # Step 8: Create a list of valid keys
         valid_keys = [k for k, c, s in context if "Not applicable" not in s]
+        # Step 9: If there are valid keys, add them to the context string
         if len(valid_keys) > 0:
             context_str += "\n\nValid keys: " + ", ".join(valid_keys)
+        # Step 10: Return the context string and the context
         return context_str, {k: c for k, c, s in context}
+
 
     def query(
         self,
@@ -131,17 +188,35 @@ class Docs:
         k: int = 5,
         max_sources: int = 5,
         length_prompt: str = "about 100 words",
-    ):
+    ) -> Answer:
+        """
+        A method to query the collection.
+        We first get the evidence for the question.
+        We then use the QA chain to get the answer.
+        We then use the edit chain to edit the answer if it is truncated.
+        We then return the answer.
+        
+        Args:
+            query (str): The question to be answered.
+            k (int, optional): The number of documents to be returned. Defaults to 5.
+            max_sources (int, optional): The maximum number of sources to be returned. Defaults to 5.
+            length_prompt (str, optional): The length prompt to be used for the QA chain. Defaults to "about 100 words".
+        """
+        # Get the context and citations from the evidence.
         context_str, citations = self.get_evidence(query, k=k, max_sources=max_sources)
         bib = dict()
+        # Check if there is enough information to answer the question.
         if len(context_str) < 10:
             answer = "I cannot answer this question due to insufficient information."
+        # If there is enough information, get the answer.
         else:
             answer = qa_chain.run(
                 question=query, context_str=context_str, length=length_prompt
             )[1:]
+            # If the answer is truncated, edit it.
             if maybe_is_truncated(answer):
                 answer = edit_chain.run(question=query, answer=answer)
+        # Add the citations to the answer.
         for key, citation in citations.items():
             # do check for whole key (so we don't catch Callahan2019a with Callahan2019)
             skey = key.split(" ")[0]
--- paperqa/readers.py
+++ paperqa/readers.py
@@ -4,21 +4,52 @@ TextSplitter = RecursiveCharacterTextSplitter
 
 
 def parse_pdf(path, citation, key, chunk_chars=4000, overlap=50):
+    """
+    Parse a PDF file into chunks.
+
+    Parameters
+    ----------
+    path : str
+        The path to the PDF file.
+    citation : str
+        The citation for the document.
+    key : str
+        The key for the document.
+    chunk_chars : int, optional
+        The number of characters to include in each chunk.
+    overlap : int, optional
+        The number of characters to overlap between chunks.
+        
+    """
+    # Import the pypdf library
     import pypdf
 
+    # Open the pdf file
     pdfFileObj = open(path, "rb")
+    # Read the pdf file
     pdfReader = pypdf.PdfReader(pdfFileObj)
+    # Initialize the split
     splits = []
+    # Initialize the split
     split = ""
+    # Initialize the page list
     pages = []
+    # Initialize the metadata
     metadatas = []
+    # Loop through the pages in the pdf
     for i, page in enumerate(pdfReader.pages):
+        # Add the text from the page to the split
         split += page.extract_text()
+        # Add the page to the page list
         pages.append(str(i + 1))
+        # If the length of the split is greater than the chunk size
         if len(split) > chunk_chars:
+            # Add the split to the list of splits
             splits.append(split[:chunk_chars])
             # pretty formatting of pages (e.g. 1-3, 4, 5-7)
+            # Create a string for the page range
             pg = "-".join([pages[0], pages[-1]])
+            # Add the citation, key, and page range to the metadata
             metadatas.append(
                 dict(
                     citation=citation,
@@ -26,9 +57,13 @@ def parse_pdf(path, citation, key, chunk_chars=4000, overlap=50):
                     key=f"{key} pages {pg}",
                 )
             )
+            # Set the split to the last overlap characters of the split
             split = split[chunk_chars - overlap:]
+            # Reset the page list
             pages = [str(i + 1)]
+    # Close the pdf file
     pdfFileObj.close()
+    # Return the splits and metadata
     return splits, metadatas
 
 
@@ -49,8 +84,11 @@ def parse_txt(path, citation, key, chunk_chars=4000, overlap=50):
 def read_doc(path, citation, key, chunk_chars=4000, overlap=50, disable_check=False):
     """Parse a document into chunks."""
     if path.endswith(".pdf"):
+        # Parse the PDF file into chunks
         return parse_pdf(path, citation, key, chunk_chars, overlap)
     elif path.endswith(".txt"):
+        # Parse the text file into chunks
         return parse_txt(path, citation, key, chunk_chars, overlap)
     else:
+        # Raise an error if the file type is not supported
         raise ValueError(f"Unknown file type: {path} (expected .pdf or .txt).")
